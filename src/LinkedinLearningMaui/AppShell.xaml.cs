e145913f895f90bfecfb1869c0b5d2292630eef4
Christophe Gigax <christophe.gigax@outlook.com>
2022-06-28 09:53:07 +0200

    Initial commit

---
+++ src/LinkedinLearningMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkedinLearningMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/LinkedinLearningMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LinkedinLearningMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
