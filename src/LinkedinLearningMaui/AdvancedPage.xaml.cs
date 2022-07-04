a436e37da11abfc1ffb44409ebb384eab6eeb4bc
Christophe Gigax <christophe.gigax@outlook.com>
2022-07-04 17:26:51 +0200

    Ajout de la position GPS

--- src/LinkedinLearningMaui/AdvancedPage.xaml.cs
+++ src/LinkedinLearningMaui/AdvancedPage.xaml.cs
@@ -5,5 +5,20 @@ public partial class AdvancedPage : ContentPage
 	public AdvancedPage()
 	{
 		InitializeComponent();
-	}
+    }
+
+	private async void Button_Clicked(object sender, EventArgs e)
+	{
+        var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+        var _cancelTokenSource = new CancellationTokenSource();
+
+        var location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+        if (location != null)
+        {
+            LongitudeLabel.Text = location.Longitude.ToString();
+            LatitudeLabel.Text = location.Latitude.ToString();
+            AltitudeLabel.Text = location.Altitude.ToString();
+        }
+    }
 }
